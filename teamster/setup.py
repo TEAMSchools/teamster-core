c7e09295f400e933046d24048f524b93b062a417
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-11-16 16:00:52 -0600

    initial commit

---
+++ teamster/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="teamster",
+    packages=setuptools.find_packages(exclude=["teamster_tests"]),
+    install_requires=[
+        "dagster==0.13.6",
+        "dagit==0.13.6",
+        "pytest",
+    ],
+)
