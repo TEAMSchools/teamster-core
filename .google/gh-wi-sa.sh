aab777c4c1f365127749160075f96edf77da008e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-04-27 12:13:26 -0500

    init

--- .google/gh-wi-sa.sh
+++ .google/gh-wi-sa.sh
@@ -1,4 +1,10 @@
-# assign WI role to service account for repo
+GH_WORKLOAD_IDENTITY_POOL_ID=$(
+    gcloud iam workload-identity-pools describe github-pool \
+    --location=global \
+    --format="value(name)" \
+    2> /dev/null
+)
+
 gcloud iam service-accounts add-iam-policy-binding ${GCP_SERVICE_ACCOUNT} \
     --project=${GCP_PROJECT_ID} \
     --role="roles/iam.workloadIdentityUser" \
