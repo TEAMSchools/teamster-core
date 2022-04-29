1aa5d9e02a5b58a8bdf580cab7d6b1ced74657a9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-04-29 12:43:49 -0500

    fix paths

--- .dagster/dagster-cloud-workspace-sync.sh
+++ .dagster/dagster-cloud-workspace-sync.sh
@@ -1,8 +1,8 @@
-envsubst < ./.deploy/cloud-workspace-$1.yaml.tmpl > ./.deploy/cloud-workspace-$1.yaml
+envsubst < ./.dagster/cloud-workspace-$1.yaml.tmpl > ./.dagster/cloud-workspace-$1.yaml
 
 if [ $1 == prod ]
 then
     pdm run k8s-secret
 fi
 
-dagster-cloud workspace sync -d $1 -w ./.deploy/cloud-workspace-$1.yaml
+dagster-cloud workspace sync -d $1 -w ./.dagster/cloud-workspace-$1.yaml
