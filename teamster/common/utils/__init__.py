eb40a843781740e6fc87630520dde6cd5ef802ea
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-05-18 18:05:23 -0500

    use graphql client

--- teamster/common/utils/__init__.py
+++ teamster/common/utils/__init__.py
@@ -4,7 +4,8 @@ from contextlib import contextmanager
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-from dagster.core.storage.pipeline_run import RunsFilter, DagsterRunStatus
+# from dagster.core.storage.pipeline_run import RunsFilter, DagsterRunStatus
+from dagster_graphql import DagsterGraphQLClient
 
 LOCAL_TIMEZONE = ZoneInfo(os.getenv("LOCAL_TIMEZONE"))
 TODAY = datetime.now(tz=LOCAL_TIMEZONE)
@@ -24,16 +25,30 @@ def time_limit(seconds):
         signal.alarm(0)
 
 
+# def get_last_successful_schedule_run(context):
+#     runs = context.instance.get_runs(
+#         filters=RunsFilter(
+#             statuses=[DagsterRunStatus.SUCCESS],
+#             job_name=context.job_name,
+#             tags={
+#                 "dagster/schedule_name": context.get_tag(key="dagster/schedule_name")
+#             },
+#         ),
+#         limit=1,
+#     )
+
+#     return runs[0] if runs else None
+
+
 def get_last_successful_schedule_run(context):
-    runs = context.instance.get_runs(
-        filters=RunsFilter(
-            statuses=[DagsterRunStatus.SUCCESS],
-            job_name=context.job_name,
-            tags={
-                "dagster/schedule_name": context.get_tag(key="dagster/schedule_name")
-            },
-        ),
-        limit=1,
+    query = (
+        "query { runsOrError( filter: { statuses: [SUCCESS]\n"
+        f'pipelineName: "{context.job_name}"\n'
+        'tags: { key: "dagster/schedule_name"\n'
+        'value: "'
+        f'{context.get_tag(key="dagster/schedule_name")}'
+        '"}} limit: 1 ) { ... on Runs { results { startTime }}}}'
     )
 
-    return runs[0] if runs else None
+    client = DagsterGraphQLClient("localhost", port_number=3000)
+    return client._execute(query=query)
